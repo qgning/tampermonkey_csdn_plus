4e76d1a3cf249775772f2eb49e683cf97abfd2f5
n <qgning@yahoo.com>
2023-07-23 22:04:25 +0800

    add expent

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {GM_addStyle, GM_setClipboard } from '$'
+import { GM_addStyle, GM_setClipboard } from '$'
 
 //去除登录框
 // GM_addStyle(".passport-login-container{display:none!important;}");
@@ -23,37 +23,44 @@ GM_addStyle("\
     user-select: auto;\
     !important;\
 }\
-");
+")
 
 // 删除页面的ID，使其不关联相关事件
 //document.getElementById('content_views')?.setAttribute('id','')
 //document.getElementById('article_content')?.setAttribute('id','')
 
 // 使用cloneNode复制一遍.blog-content-box去除js赋值的处理程序
+
 let contentEl = document.querySelector('main .blog-content-box')
-contentEl.parentNode.replaceChild(contentEl.cloneNode(true), contentEl)
+if (contentEl && contentEl.parentNode) {
+  contentEl.parentNode.replaceChild(contentEl.cloneNode(true), contentEl)
+}
 
 // 展开折叠的代码
 document.querySelectorAll('.set-code-hide').forEach(el => el.classList.remove('set-code-hide')) //移除折叠样式
 document.querySelectorAll('.hide-preCode-box').forEach(el => el.remove())   //移除下拉箭头
 
 // 遍历登陆复制按钮并修改属性
-let copy_buttons = document.getElementsByClassName('hljs-button');
+let copy_buttons = document.getElementsByClassName('hljs-button')
 for (let i = 0; i < copy_buttons.length; i++) {
-  copy_buttons[i].setAttribute('data-title', 'Copy code');
-  copy_buttons[i].removeAttribute('onclick','');
+  copy_buttons[i].setAttribute('data-title', 'Copy code')
+  copy_buttons[i].removeAttribute('onclick')
 
-  let timerid;
+  let timerid: number | undefined = undefined // TODO
 
-  copy_buttons[i].addEventListener('click', function (event) {
-    let button = this;
+  copy_buttons[i].addEventListener('click', (event: Event) => {
+    let button = copy_buttons[i];
 
-    clearTimeout(timerid);
-    event.stopPropagation();  // 阻止事件冒泡
-    GM_setClipboard(this.parentNode.innerText);
-    button.setAttribute('data-title', 'Copied!');
-    timerid = setTimeout(function () {
-      button.setAttribute('data-title', 'Copy code');
-    }, 1000);
-  });
+    clearTimeout(timerid)
+    event.stopPropagation()   // 阻止事件冒泡
+    GM_setClipboard((button.parentNode as HTMLElement).innerText, 'text')  //TODO 去掉断言
+    button.setAttribute('data-title', 'Copied!')
+    timerid = setTimeout(() => {
+      button.setAttribute('data-title', 'Copy code')
+    }, 1000)
+  })
 }
+
+// 展开需要关注博主阅读全文的内容
+document.getElementById('article_content')?.removeAttribute("style")  //TODO 错误处理
+document.getElementById('btn-no-login')?.remove() //TODO 错误处理
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
 import { defineConfig } from 'vite';
 import monkey from 'vite-plugin-monkey';
+import { version } from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     monkey({
-      entry: 'src/main.js',
+      entry: 'src/main.ts',
       userscript: {
-        name: "csdn_copy_without_login",
-        namespace: "https://github.com/qgning/tampermonkey_csdn_copy_without_login",
-        version: "0.0.1",
-        description: "CSDN免登录复制, 保持文本原有格式",
-        author: "CGN",
+        name: "csdn_plus",
+        namespace: "https://github.com/qgning/tampermonkey_csdn_plus",
+        version,
+        description: "CSDN免登录,保持文本原有格式复制,展开折叠的代码,展开需要关注博主阅读全文的内容",
+        author: "gning",
         icon: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACoElEQVRYR+2UT2hcVRTGf99LTEGki/x5nRmrjTNDpqaUQoKO4kYQQcRFCzaLWIWKhYLYRRctdNNk1xYRSopYFFxokVriJhaLiN2kQgJuQiN2nBknEH3JpCmUYrWYeV950xRsKd1JN++uDufce853fvfcKx7x0iOuTyogJZASSAmkBO4hcHlwsOvGzTgXmKy9dr2ri5WhanXl//yu2wJ+ym/d3hGzFxgFb/pvwXLjioqZzKDxfsslwQ7gcSACmrInLJ1dP7NkWMBcaK3FpxeuXo2K3d0bvaHzPGIjpgfoNsxJPlP7c2VCM/mtbyj2aUMO+F5mzvIlSU9gb36+UTlWyIaXgW2Gc5hf2slgs3Af1reID23GEUXBc8AAMC8HI2tws0Px78A/hsnAumW5fDefZraUvkbsBn9VblRGH4Q7nw1XdUf5uQCmgo7Wj5XF1T+SvYVs30nQAcOeetQ8k/jymXBM4ijWKan1uQl+Fvq1Gi0/m8SLmd6XreBiYmumv+TEiPH4i43K2AMFZPpel7QT2AX0AteNPq5Hy0cK2fCbxN+K9UJjeXnmvgIXbE9IOg/8UIuar94X/1uzz5SmbV7CfFFeuPLOwwaumMn0Qbzb4l1gWPaIpcOJfaujq2dxcfHaHSphgrwf+YAddAp/JJisRs031+NJI4nwec1uKb1l8WUbB9SM5rCrEqWWg/E9/65+QEw3YskmksgDb7eF2nuRjgNhewbwkNAwIpdcVz1qjuRz4SmZ98Gf1aKVfW0C2U3vGX9qe6r9CmafHnjFgZIXUAblDdcEC491tnaO/HXjoB33Iw0BTyF+w8zbVOpLzbFCNpwGnmx3DA1ww+hsPWp+st7td8BrmBO1pWZCi0IuPIQ5Dj6Z/oQpgZRASiAlkBK4DZtvGMy5535NAAAAAElFTkSuQmCC',
-        supportURL: "https://github.com/qgning/tampermonkey_csdn_copy_without_login",
-        updateURL: "https://github.com/qgning/tampermonkey_csdn_copy_without_login/releases/download/latest/tampermonkey_csdn_copy_without_login.user.js",
-        downloadURL: "https://github.com/qgning/tampermonkey_csdn_copy_without_login/releases/download/latest/tampermonkey_csdn_copy_without_login.user.js",
+        supportURL: "https://github.com/qgning/tampermonkey_csdn_plus",
+        updateURL: "https://github.com/qgning/tampermonkey_csdn_plus/releases/latest/download/tampermonkey_csdn_plus.user.js",
+        downloadURL: "https://github.com/qgning/tampermonkey_csdn_plus/releases/latest/download/tampermonkey_csdn_plus.user.js",
         match: ['https://blog.csdn.net/*/article/details/*'],
         grant: [
           'GM_addStyle',
